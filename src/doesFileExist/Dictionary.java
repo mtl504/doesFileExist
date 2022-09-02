ecd1beec1cca79167b8b09a632d4ca2568e63016
mtl504 <xminhx504@yahoo.com>
2022-09-02 15:25:38 -0500

    init commit

---
+++ src/doesFileExist/Dictionary.java
@@ -0,0 +1,34 @@
+package doesFileExist;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Dictionary {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+
+		doesFileExist(null);
+
+	}
+
+	public static void doesFileExist(String path) throws IOException {
+		String path1 = "src\\data\\textFile";
+
+		File f = new File(path1);
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+			
+			
+		}
+
+	}
+
+}
